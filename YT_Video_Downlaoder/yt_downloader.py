535e819a214d70d8f345711abb11fec2173e91c8
mrsarthi <wforsarthi_biness@outlook.com>
2023-04-13 10:10:54 +0530

    Significant change

--- YT_Video_Downlaoder/yt_downloader.py
+++ YT_Video_Downlaoder/yt_downloader.py
@@ -1,4 +1,25 @@
 from pytube import YouTube
 
-a = 90+34
-print(a)
+link = input("Enter the link to download: ")
+yt = YouTube(link)
+print(yt.title)
+# print(yt.streams.filter(progressive=True))
+
+stream = yt.streams.get_by_itag(22)
+stream.download()
+
+
+
+# from sys import argv
+
+# link = input("Enter the link to download: ")
+# yt = YouTube(link)
+
+# print("Title: ", yt.title)
+
+# print("View: ", yt.views)
+
+# yd = yt.streams.get_highest_resolution()
+
+# # ADD FOLDER HERE
+# yd.download('./YTfolder')
